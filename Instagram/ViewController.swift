e868e11c4f6adb97a3031194222ac09ce1cba5ac
alexmeigz <alexmei@ucsb.edu>
2020-11-17 21:22:04 -0800

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Alex Mei on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
